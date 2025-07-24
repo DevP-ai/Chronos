cd9c84ff1aed461cb8cb0f04efe26ef1ea260749
Devajit <patar1447@gmail.com>
2025-07-24 17:14:56 +0530

    Reminder Item fetching from api and show in ReminderScreen

--- app/src/main/java/com/dev/ai/app/chronos/presentation/ui/ReminderScreen.kt
+++ app/src/main/java/com/dev/ai/app/chronos/presentation/ui/ReminderScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
@@ -18,6 +20,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -30,6 +33,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.dev.ai.app.chronos.presentation.ui.component.ReminderItem
 import com.dev.ai.app.chronos.presentation.ui.component.ThemeSwitcher
 import com.dev.ai.app.chronos.presentation.viewModel.ReminderViewModel
 import com.dev.ai.app.chronos.ui.theme.ChronosTheme
@@ -39,6 +43,8 @@ import com.dev.ai.app.chronos.ui.theme.ChronosTheme
 fun ReminderScreen(
     viewModel: ReminderViewModel = hiltViewModel()
 ) {
+    val reminders by viewModel.reminders.collectAsState()
+
     var isDarkTheme by remember { mutableStateOf(false) }
     var promptText by remember { mutableStateOf("") }
     var showDialog by remember { mutableStateOf(false) }
@@ -55,7 +61,7 @@ fun ReminderScreen(
                         ){
                             Text(
                                 text = "Chronos",
-                                fontSize = 24.sp,
+                                fontSize = 32.sp,
                                 fontWeight = FontWeight.Bold,
                                 textAlign = TextAlign.Center
                             )
@@ -110,7 +116,7 @@ fun ReminderScreen(
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(horizontal = 16.dp, vertical = 4.dp),
+                        .padding(horizontal = 12.dp, vertical = 4.dp),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     OutlinedTextField(
@@ -119,7 +125,7 @@ fun ReminderScreen(
                         label = { Text("Enter prompt (e.g., birthday wish)") },
                         modifier = Modifier
                             .weight(1f)
-                            .padding(end = 8.dp)
+                            .padding(end = 2.dp)
                     )
                     IconButton(
                         onClick = {
@@ -146,6 +152,28 @@ fun ReminderScreen(
                             showDialog = false
                         }
                     )
+                }
+                Text(
+                    text = "Reminders",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 12.dp, vertical = 8.dp)
+                )
+                LazyColumn {
+                    items (reminders){reminder->
+                        ReminderItem(
+                            title = reminder.title,
+                            notes = reminder.notes,
+                            dateTime = reminder.dateTime,
+                            imageUrl = reminder.imageUrl,
+                            onEdit = {
+                                showDialog = false
+                            },
+                            onDelete = {}
+                        )
+                    }
                 }
             }
         }
