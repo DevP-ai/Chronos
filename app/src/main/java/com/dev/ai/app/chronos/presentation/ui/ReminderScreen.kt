cc63f87672d3188b3071b74d40bd3264ad311d8b
Devajit <patar1447@gmail.com>
2025-07-24 12:57:26 +0530

    Custom Toggle implemented and added profile icon

--- app/src/main/java/com/dev/ai/app/chronos/presentation/ui/ReminderScreen.kt
+++ app/src/main/java/com/dev/ai/app/chronos/presentation/ui/ReminderScreen.kt
@@ -2,14 +2,14 @@ package com.dev.ai.app.chronos.presentation.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -24,6 +24,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.dev.ai.app.chronos.presentation.ui.component.ThemeSwitcher
 import com.dev.ai.app.chronos.ui.theme.ChronosTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -57,14 +58,17 @@ fun ReminderScreen(
                     },
                     navigationIcon ={
                         IconButton(onClick = {}) {
-                            Icon(Icons.Default.ExitToApp, contentDescription = "Logout")
+                            Icon(
+                                Icons.Default.AccountCircle,
+                                modifier = Modifier.size(36.dp),
+                                contentDescription = "Logout"
+                            )
                         }
                     },
                     actions = {
-                        Switch(
-                            checked = isDarkTheme,
-                            onCheckedChange ={isDarkTheme = it},
-                            modifier = Modifier.padding(end = 8.dp)
+                        ThemeSwitcher(
+                            darkTheme = isDarkTheme,
+                            onClick = { isDarkTheme = !isDarkTheme }
                         )
                     }
                 )
