5d6b3a1fba577663ca857c844da8c3be2ccbb7b1
Devajit <patar1447@gmail.com>
2025-07-24 13:24:24 +0530

    Floating Action Button Added

--- app/src/main/java/com/dev/ai/app/chronos/presentation/ui/ReminderScreen.kt
+++ app/src/main/java/com/dev/ai/app/chronos/presentation/ui/ReminderScreen.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -72,6 +74,14 @@ fun ReminderScreen(
                         )
                     }
                 )
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = {}) {
+                    Icon(
+                        Icons.Default.Add,
+                        contentDescription = "Add Reminder"
+                    )
+                }
             }
         ){padding->
 
