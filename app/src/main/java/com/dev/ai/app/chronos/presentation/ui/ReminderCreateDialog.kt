b9574c177cc7fb42931bc93e16be72f7763750f2
Devajit <patar1447@gmail.com>
2025-07-25 00:03:37 +0530

    Image view content scale added

--- app/src/main/java/com/dev/ai/app/chronos/presentation/ui/ReminderCreateDialog.kt
+++ app/src/main/java/com/dev/ai/app/chronos/presentation/ui/ReminderCreateDialog.kt
@@ -39,6 +39,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -255,6 +256,7 @@ fun ReminderCreateDialog(
                     AsyncImage(
                         model = localImageUri,
                         contentDescription = null,
+                        contentScale = ContentScale.Crop,
                         modifier = Modifier
                             .size(80.dp)
                             .clip(CircleShape)
@@ -263,6 +265,7 @@ fun ReminderCreateDialog(
                     AsyncImage(
                         model = state.imageUrl,
                         contentDescription = null,
+                        contentScale = ContentScale.Crop,
                         modifier = Modifier
                             .size(80.dp)
                             .clip(CircleShape)
--- app/src/main/java/com/dev/ai/app/chronos/presentation/ui/component/ReminderItem.kt
+++ app/src/main/java/com/dev/ai/app/chronos/presentation/ui/component/ReminderItem.kt
@@ -17,7 +17,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.autofill.ContentType
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
@@ -49,6 +51,7 @@ fun ReminderItem(
                 AsyncImage(
                     model = imageUrl,
                     contentDescription = null,
+                    contentScale = ContentScale.Crop,
                     modifier = Modifier
                         .size(56.dp)
                         .clip(CircleShape)
